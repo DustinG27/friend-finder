e603200e29ea0d1963eb8373d677d54c75c9ba34
dustin george <DustinG27@gmail.com>
2020-05-07 20:37:23 -0400

    x button closes out of drummer window

--- app/data/friends.js
+++ app/data/friends.js
@@ -8,25 +8,25 @@ var friends = [
   {
     name: "derek",
     photo:
-      "https://images.unsplash.com/photo-1518499845966-9a86ddb68051?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80",
+      "https://www.bhphotovideo.com/images/images2500x2500/behringer_rd_8_rhythm_designer_analog_drum_1501408.jpg",
     scores: [1, 3, 5, 1, 4, 1, 5, 5, 5, 1],
   },
   {
     name: "patrick",
     photo:
-      "https://images.unsplash.com/photo-1518499845966-9a86ddb68051?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80",
+      "https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F20%2F2014%2F06%2Fweezer-600.jpg&q=85",
     scores: [1, 5, 3, 3, 3, 5, 4, 3, 1, 5],
   },
   {
     name: "travis",
     photo:
-      "https://images.unsplash.com/photo-1518499845966-9a86ddb68051?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80",
+      "https://artist-assets.hubbardradio.com/travisbarker-5_v1000.jpg",
     scores: [5, 3, 2, 5, 5, 4, 5, 5, 4, 5],
   },
   {
-    name: "brandon",
+    name: "kat",
     photo:
-      "https://images.unsplash.com/photo-1518499845966-9a86ddb68051?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80",
+      "https://www.vmcdn.ca/f/files/via/import/2019/06/12074852_kelly-watson-schutz-fundraiser.jpg;w=960",
     scores: [5, 3, 1, 4, 4, 5, 4, 3, 3, 3],
   },
 ];
